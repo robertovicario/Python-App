77997871519d722edeacb557a13ca9c26849cd3c
Roberto Vicario <119845903+robertovicario@users.noreply.github.com>
2025-05-22 08:45:27 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask_frozen import Freezer
-import sys
 
 from routes.index import index_bp
 
@@ -8,14 +6,8 @@ from routes.index import index_bp
 
 app = Flask(__name__)
 app.register_blueprint(index_bp)
-app.config['FREEZER_RELATIVE_URLS'] = True
-
-freezer = Freezer(app)
 
 # -------------------------
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'build':
-        freezer.freeze()
-    else:
-        app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
