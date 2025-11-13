660b4cb25a53271e932bc96b4b06b9dab4b85c62
Roberto Vicario <119845903+robertovicario@users.noreply.github.com>
2025-11-13 22:10:32 +0100

    Refactor comments in app.py for clarity
    Refactor comments for better readability and organization.

--- app/app.py
+++ app/app.py
@@ -1,25 +1,25 @@
-# -------------------------
+# =========================
 # Dependencies
-# -------------------------
+# =========================
 
 from flask import Flask
 import json
 import os
 
-# -------------------------
-
 from routes.fallback import fallback_bp
 from routes.index import index_bp
 
-# -------------------------
-# Flask Settings
-# -------------------------
+# =========================
+# Flask
+# =========================
 
 app = Flask(__name__)
+app.register_blueprint(fallback_bp)
+app.register_blueprint(index_bp)
 
-# -------------------------
-# Configuration Settings
-# -------------------------
+# =========================
+# Configuration
+# =========================
 
 config_path = os.path.join(os.path.dirname(__file__), 'config', 'settings.json')
 with open(config_path, 'r') as f:
@@ -29,16 +29,9 @@ with open(config_path, 'r') as f:
 def config_json():
     return dict(config=config)
 
-# -------------------------
-# Blueprints Registration
-# -------------------------
-
-app.register_blueprint(fallback_bp)
-app.register_blueprint(index_bp)
-
-# -------------------------
-# Starting Point
-# -------------------------
+# =========================
+# Entry Point
+# =========================
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7860)
